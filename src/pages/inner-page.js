87ce8a8738e76557a578886a4df1c3bac5c794f3
mash025 <mashhoodsheikh7@gmail.com>
2024-07-26 16:35:11 +0500

    Changes on Detail inner page, contact page, acadmic offerings and stylesheer.

--- src/pages/inner-page.js
+++ src/pages/inner-page.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
-//component
+// Component
 import Fancybox from "../components/FancyBox";
 
-//css
-import "..//assets/css/stylesheet.css";
+// CSS
+import "../assets/css/stylesheet.css";
 
-//images
-import banner from "..//assets/images/inner-event/inner-event-banner.webp";
-import im1 from "..//assets/images/inner-event/inner-event-1.webp";
-import im2 from "..//assets/images/inner-event/inner-event-2.webp";
-import im3 from "..//assets/images/inner-event/inner-event-3.webp";
-import im4 from "..//assets/images/inner-event/inner-event-4.webp";
-import im5 from "..//assets/images/inner-event/inner-event-5.webp";
-import im6 from "..//assets/images/inner-event/inner-event-6.webp";
+// Images
+import banner from "../assets/images/inner-event/inner-event-banner.webp";
+import im1 from "../assets/images/inner-event/inner-event-1.webp";
+import im2 from "../assets/images/inner-event/inner-event-2.webp";
+import im3 from "../assets/images/inner-event/inner-event-3.webp";
+import im4 from "../assets/images/inner-event/inner-event-4.webp";
+import im5 from "../assets/images/inner-event/inner-event-5.webp";
+import im6 from "../assets/images/inner-event/inner-event-6.webp";
 
 function Events_Inner() {
   return (
@@ -23,108 +23,68 @@ function Events_Inner() {
         <title> - Center for South Asian Music</title>
       </Helmet>
 
-      {/* header */}
-      <div className="event-inner-header">
-        <div className="event-inner-head-pic">
-          <img src={banner} alt="image" className="event-inner-head-pic" />
-        </div>
-        <div className="container event-inner-header-content">
-          <span> Andaaz: The Dance Concert </span>
-          <span>
-            <p>Dated: 30/Jun/2020</p>
-          </span>
+      {/* Header */}
+      <div className="event-inner-header" style={{ backgroundImage: `url(${banner})` }}>
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-10">
+              <div className="bannerHead">
+                <h1 className="globalHeading">Andaaz: The Dance Concert</h1>
+                <span className="dateonBanner">Dated: 30/Jun/2020</span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
-      {/* main sec */}
-
+      {/* Main Section */}
       <section className="mrSec padSec">
         <div className="container">
           <div className="row justify-content-center">
-            <div className="col-lg-10">
-              <p>
-                Weaving togather strands of classical art forms of the region,
-                Andaaz concert was a spelbinding display of Magical Khatak,
-                featuring Farah Yaisn Shaikh, a renowned performer,
-                choreographer, instructor and Founder/Director for Noorani
-                Dance, known for her evocative story telling, technical
-                precision, delicacy and grace. Accompained by Shehroze Hussain,
-                the prodigious son and deciple of Ustaad Sajid Hussain, and
-                Yosuf Kerai, Habib University's in-huose-tabla-nawaz, a
-                memorable performance was delivered.
-              </p>
+            <div className="col-lg-12">
+              <div className="bannerInnerDetais">
+                <p>
+                  Weaving together strands of classical art forms of the region,
+                  Andaaz concert was a spellbinding display of Magical Khatak,
+                  featuring Farah Yaisn Shaikh, a renowned performer,
+                  choreographer, instructor and Founder/Director for Noorani
+                  Dance, known for her evocative storytelling, technical
+                  precision, delicacy and grace. Accompanied by Shehroze Hussain,
+                  the prodigious son and disciple of Ustaad Sajid Hussain, and
+                  Yosuf Kerai, Habib University's in-house-tabla-nawaz, a
+                  memorable performance was delivered.
+                </p>
+              </div>
+              <Fancybox
+                options={{
+                  Carousel: {
+                    infinite: false,
+                  },
+                }}
+              >
+                <div className="gallery-container">
+                  <a data-fancybox="gallery" href={im1}>
+                    <img src={im1} width="" height="" alt="Event 1" />
+                  </a>
+                  <a data-fancybox="gallery" href={im2}>
+                    <img src={im2} width="" height="" alt="Event 2" />
+                  </a>
+                  <a data-fancybox="gallery" href={im3}>
+                    <img src={im3} width="" height="" alt="Event 3" />
+                  </a>
+                  <a data-fancybox="gallery" href={im4}>
+                    <img src={im4} width="" height="" alt="Event 4" />
+                  </a>
+                  <a data-fancybox="gallery" href={im5}>
+                    <img src={im5} width="" height="" alt="Event 5" />
+                  </a>
+                  <a data-fancybox="gallery" href={im6}>
+                    <img src={im6} width="" height="" alt="Event 6" />
+                  </a>
+                </div>
+              </Fancybox>
             </div>
           </div>
-          <Fancybox
-            options={{
-              Carousel: {
-                infinite: false,
-              },
-            }}
-          >
-            <div className="gallery-container">
-            <a
-              data-fancybox="gallery"
-              href={im1}
-            >
-              <img
-                src={im1}
-                width="350"
-                height="150"
-              />
-            </a>
-            <a
-              data-fancybox="gallery"
-              href={im2}
-            >
-              <img
-                src={im2}
-                width="350"
-                height="150"
-              />
-            </a>
-            <a
-              data-fancybox="gallery"
-              href={im3}
-            >
-              <img
-                src={im3}
-                width="350"
-                height="150"
-              />
-            </a>
-            <a
-              data-fancybox="gallery"
-              href={im4}
-            >
-              <img
-                src={im4}
-                width="350"
-                height="350"
-              />
-            </a>
-            <a
-              data-fancybox="gallery"
-              href={im5}
-            >
-              <img
-                src={im5}
-                width="350"
-                height="150"
-              />
-            </a>
-            <a
-              data-fancybox="gallery"
-              href={im6}
-            >
-              <img
-                src={im6}
-                width="350"
-                height="150"
-              />
-            </a>
-            </div>
-          </Fancybox>
         </div>
       </section>
     </>
